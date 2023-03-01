4f4ea99e07f38a7d5399d069b93b00e247bb233f
Mee Cha <mee.cha.nism@gmail.com>
2023-02-28 18:29:12 -0800

    Add in type

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,9 +1,5 @@
 import { prisma } from "~/db.server";
-
-type Post = {
-  slug: string;
-  title: string;
-};
+import type { Post } from "@prisma/client";
 
 export async function getPosts(): Promise<Array<Post>> {
   return prisma.post.findMany();
@@ -13,6 +9,8 @@ export async function getPost(slug: string) {
   return prisma.post.findUnique({ where: { slug } });
 }
 
-export async function createPost(post) {
+export async function createPost(
+  post: Pick<Post, "slug" | "title" | "markdown">
+) {
   return prisma.post.create({ data: post });
 }
